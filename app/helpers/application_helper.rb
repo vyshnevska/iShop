c0d628b0375c24309a69d7ea02c258ebeb5b4eab
vyshnevska <vyshnevska.n@gmail.com>
2015-05-26 16:09:11 +0300

    Better styled Cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,18 @@
 module ApplicationHelper
   def bootstrap_class_for flash_type
-    { :success => 'alert-success',
-      :error   => 'alert-danger',
-      :alert   => 'alert-warning',
-      :notice  => 'alert-info'
+    { success: 'alert-success',
+      error:   'alert-danger',
+      alert:   'alert-warning',
+      notice:  'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
   end
 
   def btn_class_for btn_type
-    { new: 'btn btn-primary',
-      edit: 'btn btn-info',
-      destroy: 'btn btn-danger',
-      default: 'btn btn-default',
-      empty: 'btn btn-warning',
-      add_to_cart: 'btn btn-warning'
+    { new:      'btn btn-primary',
+      edit:     'btn btn-info',
+      destroy:  'btn btn-danger',
+      default:  'btn btn-default',
+      cart:    'btn btn-warning'
     }[btn_type] || 'btn btn-default'
   end
 
@@ -21,5 +20,10 @@ module ApplicationHelper
     { destroy: 'btn btn-primary btn-xs',
       default: 'btn btn-default btn-xs'
     }[btn_type] || 'btn btn-default btn-xs'
+  end
+
+  def hidden_div_if condition, attributes = {}, &block
+    attributes['style'] = 'display: none' if condition
+    content_tag("div", attributes, &block)
   end
 end
