472ae0d63c80ad26441a590ba6a82d4f87965cca
Nina Vyshnevska <vyshnevska.n@gmail.com>
2015-06-05 10:22:56 +0300

    Devise

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'sass-rails', '~> 4.0.3'
 gem 'twitter-bootstrap-rails'
 gem 'quiet_assets'
 
+gem 'devise'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
   before_action :set_session_cart
 
   private
