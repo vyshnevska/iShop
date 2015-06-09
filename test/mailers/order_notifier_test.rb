ccbc0414bf45758cf17c8752c11ed1cace3acd18
Nina Vyshnevska <vyshnevska.n@gmail.com>
2015-06-09 23:59:57 +0300

    Fixed OrderNotifierTest

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
+require 'pry'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received(orders(:one))
+    mail = OrderNotifier.received(orders(:paid_by_check))
     assert_equal I18n.t('mails.order_notifier.received.subject'), mail.subject
     assert_equal ["dave1@example.org"], mail.to
     assert_equal ["vyshnevska.n@gmail.com"], mail.from
@@ -10,11 +11,11 @@ class OrderNotifierTest < ActionMailer::TestCase
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped(orders(:one))
+    mail = OrderNotifier.shipped(orders(:paid_by_check))
     assert_equal I18n.t('mails.order_notifier.shipped.subject'), mail.subject
     assert_equal ["dave1@example.org"], mail.to
     assert_equal ["vyshnevska.n@gmail.com"], mail.from
-    assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby<\/td>/, mail.body.encoded
+    assert_match /1 x Programming Ruby/, mail.body.encoded
   end
 
 end
